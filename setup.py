41f9c71563cb0834984005dc513588eded399925
VitorRussomanoCGCOMPASS <Vitor.Ibanez@cgcompass.com>
2023-04-24 14:59:53 -0300

    packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="wheels_airflow",
+    version="0.1.0",
+    url="https://github.com/VitorRussomanoCGCOMPASS/wheels_airflow.git",
+    author="VitorRussomanoCGCOMPASS",
+    author_email="VitorIbanez@cgcompass.com",
+    packages=[
+        "abstract_objs",
+    ],
+)
